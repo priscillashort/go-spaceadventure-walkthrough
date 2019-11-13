b7e778894393469f8c3a79dbda41cbfb5d2c9a1f
priscillashort <pshort2@cocc.edu>
2019-11-12 16:10:34 -0800

    Add Planet object and use in PlanetarySystem object

--- cmd/spaceadventure/main.go
+++ cmd/spaceadventure/main.go
@@ -3,5 +3,12 @@ package main
 import "github.com/priscillashort/go-spaceadventure-walkthrough/internal/spaceadventure"
 
 func main() {
-	spaceadventure.Start(spaceadventure.PlanetarySystem{"Solare System"})
+	spaceadventure.Start(spaceadventure.PlanetarySystem{
+		Name: "Solar System", 
+		Planets: []spaceadventure.Planet{
+			spaceadventure.Planet{
+				Name: "Jupiter", Description: "Desert planet",
+			},
+		},
+	})
 }
